cb0fe042ad83af59eededf1d6a6ff63129bc9e01
TLORENT <lorent.tanguy@gmail.com>
2022-05-04 23:39:25 +0200

    [menu_commons] add template struct Menu

--- src/ihm/menus/menu_commons.hpp
+++ src/ihm/menus/menu_commons.hpp
@@ -4,6 +4,20 @@
 #include <map>
 
 template<typename T>
+struct Menu {
+    const std::map<T, std::string> _labels;
+    const std::map<T, char>        _commands;
+    const std::initializer_list<T> _options;
+
+    Menu(const std::map<T, std::string>& labels,
+         const std::map<T, char>&        commands,
+         const std::initializer_list<T>& options)
+        : _labels(labels), _commands(commands), _options(options)
+    {
+    }
+};
+
+template<typename T>
 bool menu_contains_command(const std::initializer_list<T>& list_of_options, const std::map<T, char>& list_of_commands, const char command)
 {
     for (auto command_name : list_of_options) {
