4bc9612ee069ed4ecbe40faf5bfb5125eb2313ed
TLORENT <lorent.tanguy@gmail.com>
2022-05-14 18:36:45 +0100

    [main.cpp] removing box-muller test

--- src/main.cpp
+++ src/main.cpp
@@ -10,33 +10,28 @@
 
 int main()
 {
-    while (true) {
-        std::cout << box_muller(10, 1.75);
-        wait_for_any_key_pressed();
+    Character player;
+    bool      quit = false;
+
+    while (!quit) {
+        show_main_menu();
+        const char command = get_command_from_user<MainMenu>(main_menu, &show_main_menu);
+
+        switch (command) {
+        case static_cast<int>(MainMenu::New_game):
+            player = create_new_character();
+            clear_console();
+            player.introduce_themself();
+            wait_for_any_key_pressed();
+            break;
+        case static_cast<int>(MainMenu::Quit):
+            quit = true;
+            clear_console();
+            display_text("See you ;)");
+            break;
+        default:
+            std::cout << "Error [main.cpp] : Wrong command entered.\n";
+            break;
+        }
     }
-
-    // Character player;
-    // bool      quit = false;
-
-    // while (!quit) {
-    //     show_main_menu();
-    //     const char command = get_command_from_user<MainMenu>(main_menu, &show_main_menu);
-
-    //     switch (command) {
-    //     case static_cast<int>(MainMenu::New_game):
-    //         player = create_new_character();
-    //         clear_console();
-    //         player.introduce_themself();
-    //         wait_for_any_key_pressed();
-    //         break;
-    //     case static_cast<int>(MainMenu::Quit):
-    //         quit = true;
-    //         clear_console();
-    //         display_text("See you ;)");
-    //         break;
-    //     default:
-    //         std::cout << "Error [main.cpp] : Wrong command entered.\n";
-    //         break;
-    //     }
-    // }
 }
